11e120b4eb188039ffc2bbedc1eae31e4f197785
Christophe Farges <c.farges@groupeonepoint.com>
2017-12-22 11:25:40 +0100

    add stuff

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route("/")
+all_data=[]
+
+@application.route("/", methods=['POST'])
 def hello():
-    return "Hello World!"
+    data = request.form
+    all_data.append(data)
+    return all_data
 
 if __name__ == "__main__":
     application.run()
