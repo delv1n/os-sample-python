2abce437e864cfc033663322bb29c764a7ab5f9c
Christophe Farges <c.farges@groupeonepoint.com>
2017-12-22 11:31:51 +0100

    add stuff

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import json
 application = Flask(__name__)
 
 all_data=[]
@@ -6,11 +7,11 @@ all_data=[]
 @application.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
-        return all_data
+      return json.dumps(all_data)
     else:
       data = request.json
       all_data.append(data)
-      return all_data
+      return json.dumps(all_data)
 
 if __name__ == "__main__":
     application.run()
