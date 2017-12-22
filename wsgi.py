f274d5ff2ee3da79745ee25fb2593d81f4c3fe30
Christophe Farges <c.farges@groupeonepoint.com>
2017-12-22 11:56:05 +0100

    add stuff

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,11 @@ all_data=[]
 @application.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
-      return str(json.dumps(all_data))
+      return str(json.dumps(str(all_data)))
     else:
       data = request.data
       all_data.append(data)
-      return str(json.dumps(all_data))
+      return str(json.dumps(str(all_data)))
 
 if __name__ == "__main__":
     application.run()
